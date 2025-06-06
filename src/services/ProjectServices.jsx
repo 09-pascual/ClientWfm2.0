1bad7640db4fda464937aff922a3d01685356c4f
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2025-06-05 23:44:53 -0400

    adding service to retrieve all projects

---
+++ src/services/ProjectServices.jsx
@@ -0,0 +1,9 @@
+export const getAllProjects = () => {
+  return fetch("http://localhost:8000/projects", {
+    headers: {
+      Authorization: `Token ${
+        JSON.parse(localStorage.getItem("workflow_token")).token
+      }`,
+    },
+  }).then((response) => response.json());
+}; 
